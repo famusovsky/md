dd4047023e93d6938705a87b8a724abb844544c7
Alexey Stepanov <st3p-by-step@ya.ru>
2023-07-10 17:34:15 +0300

    Getting dsn from ENV implemented

--- pkg/db/db.go
+++ pkg/db/db.go
@@ -1,8 +1,16 @@
 package db
 
-import "database/sql"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+)
 
 func OpenDB(dsn string) (*sql.DB, error) {
+	if dsn == "" {
+		dsn = getDsnFromEnv()
+	}
+
 	db, err := sql.Open("postgres", dsn)
 	if err != nil {
 		return nil, err
@@ -12,4 +20,11 @@ func OpenDB(dsn string) (*sql.DB, error) {
 	}
 
 	return db, nil
+}
+
+func getDsnFromEnv() string {
+	dsn := fmt.Sprintf("port=%s user=%s password=%s dbname=%s sslmode=%s host=%s",
+		os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), os.Getenv("DB_SSLMODE"), os.Getenv("DB_HOST"))
+
+	return dsn
 }
