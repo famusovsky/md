aca891e851c68cd21e170215a6d3311fe23f8aa7
Alexey Stepanov <st3p-by-step@ya.ru>
2023-07-10 12:56:52 +0300

    CSS usage fix

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 )
 
 func (app *application) routes() *chi.Mux {
 	mux := chi.NewRouter()
+
+	mux.Use(middleware.Logger)
+	mux.Use(middleware.Recoverer)
+
 	mux.Get("/", app.home)
 	mux.Get("/{note}", app.showNote)
 	mux.Get("/favicon.ico", app.favicon)
@@ -16,7 +21,7 @@ func (app *application) routes() *chi.Mux {
 
 	fileServer := http.FileServer(http.Dir("ui/static"))
 	mux.Handle("/static", http.NotFoundHandler())
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
 
 	return mux
 }
